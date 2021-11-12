1d7eff925afbad8318ef803ba501fa8f5e7fc1d5
squid_ro <squid_ro@edge-core.com>
2021-11-12 17:15:01 +0000

    Add default-cfg method.

--- util/__init__.py
+++ util/__init__.py
@@ -1,5 +1,5 @@
 
 __all__ = [ 'util_db', 'util_utl', 'util_method_tbl', 'util_vlan',
             'util_platform', 'util_interface', 'util_lldp', 'util_timer',
-            'util_acl', 'util_fdb', 'util_nbr'
+            'util_acl', 'util_fdb', 'util_nbr', 'util_dcfg',
           ]
--- util/util_dcfg.py
+++ util/util_dcfg.py
@@ -7,11 +7,39 @@
 import util_utl, util_method_tbl, pdb
 from xml.etree import cElementTree as ET
 
+CFG_TMPL_HOSTNAME_CMD = "config hostname {new_host}"
+CFG_TMPL_DATE_CMD     = "date -s '{new_date_time}'"
+
 
 #
 # set functions
 #
 
+#
+# ex: <entry><method>default-cfg</method><host>xmpp2</host>
+#            <date>20211112 17:07:26</date></entry>
+@util_utl.utl_dbg
+def dcfg_default_cfg(ent_elm, db_args):
+    is_host_ok = is_date_ok = False
+
+    date_elm = ent_elm.find("date")
+    if None not in [ date_elm, date_elm.text ]:
+        exe_cmd = CFG_TMPL_DATE_CMD.format(new_date_time = date_elm.text)
+        is_date_ok = util_utl.utl_execute_cmd(exe_cmd)
+
+    host_elm = ent_elm.find("host")
+    if None not in [ host_elm, host_elm.text ]:
+        exe_cmd = CFG_TMPL_HOSTNAME_CMD.format(new_host = host_elm.text)
+        is_host_ok = util_utl.utl_execute_cmd(exe_cmd)
+
+    if not is_host_ok:
+        util_method_tbl.mtbl_append_retmsg(ent_elm, "INVALID HOSTNAME")
+
+    if not is_date_ok:
+        util_method_tbl.mtbl_append_retmsg(ent_elm, "INVALID DATE")
+
+    if is_date_ok and is_host_ok:
+        util_method_tbl.mtbl_append_retmsg(ent_elm, "SUCCESS")
 
 #
 # get functions 
@@ -21,4 +49,4 @@ from xml.etree import cElementTree as ET
 #
 # register related functions to method table
 #
-#util_method_tbl.mtbl_register_method('vlan-member', vlan_mbr)
+util_method_tbl.mtbl_register_method('default-cfg', dcfg_default_cfg)
